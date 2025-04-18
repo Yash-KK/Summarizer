fb72b5fa2fa60d7ad008db3c6301fc7b0259b4a7
Yash-KK <reachyashk@gmail.com>
2025-04-18 17:01:09 +0530

    lp:pricing-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
 import HowItWorksSection from "@/components/home/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <DemoSection />
       <HowItWorksSection />
+      <PricingSection />
     </div>
   );
 }
